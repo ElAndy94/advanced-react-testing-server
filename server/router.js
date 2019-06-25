d53bdd8fc54f5b392314db63347fb7b10231d5c9
Andrew <andrewpeliza@hotmail.com>
2019-06-25 17:53:42 +0100

    Authentication Boilerplate

--- server/router.js
+++ server/router.js
@@ -6,6 +6,9 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = app => {
+  app.get('/', requireAuth, (req, res) => {
+    res.send({ hi: 'there' });
+  });
   app.post('/signin', requireSignin, Authentication.signin);
   app.post('/signup', Authentication.signup);
 };
