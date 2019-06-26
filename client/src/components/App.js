cce4ed9ad04f810a7a9f578a7f68d06bcf6bb0d3
Andrew <andrewpeliza@hotmail.com>
2019-06-26 19:27:25 +0100

    Header / Importing React Router into project.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import Header from './Header';
+
 export default () => {
-  return <div>Hi, I am the app</div>;
+  return (
+    <React.Fragment>
+      <Header />
+      Hi, I am the app
+    </React.Fragment>
+  );
 };
