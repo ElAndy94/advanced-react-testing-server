3d60658b810c749336fe633a9986e5a0fa34bf51
Andrew <andrewpeliza@hotmail.com>
2019-07-02 15:35:40 +0100

    More Redux - Signup

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
-function Signup() {
+function Signup(props) {
+  const onSubmit = formProps => {
+    props.signup(formProps);
+  };
+
+  const { handleSubmit } = props;
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <fieldset>
         <label>Email</label>
         <Field name='email' type='text' component='input' autoComplete='none' />
@@ -17,8 +26,15 @@ function Signup() {
           autoComplete='none'
         />
       </fieldset>
+      <button>Sign Up!</button>
     </form>
   );
 }
 
-export default reduxForm({ form: 'signup' })(Signup);
+export default compose(
+  connect(
+    null,
+    actions
+  ),
+  reduxForm({ form: 'signup' })
+)(Signup);
